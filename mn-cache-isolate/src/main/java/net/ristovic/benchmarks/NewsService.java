ad28039f074ee5f831cf3b77d54e7e6e74d139cb
Ivan Ristovic <ivan.ristovic@oracle.com>
2025-02-10 12:44:33 +0100

    Init

--- mn-cache-isolate/src/main/java/net/ristovic/benchmarks/NewsService.java
+++ mn-cache-isolate/src/main/java/net/ristovic/benchmarks/NewsService.java
@@ -6,6 +6,9 @@ import io.micronaut.cache.annotation.CachePut;
 import io.micronaut.cache.annotation.Cacheable;
 
 import jakarta.inject.Singleton;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,25 +18,69 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.graalvm.nativeimage.ObjectSnapshots;
+import org.graalvm.nativeimage.ObjectSnapshots.ObjectSnapshot;
+import org.graalvm.nativeimage.ObjectSnapshots.ObjectSnapshotProvider;
+import org.graalvm.nativeimage.ObjectSnapshots.ObjectSnapshotRegion;
+import org.graalvm.nativeimage.ObjectSnapshots.ObjectSnapshotSlot;
+
 @Singleton
 @CacheConfig("headlines")
 public class NewsService {
 
+    private final Headlines cache;
+
+    public NewsService() {
+        var provider = ObjectSnapshots.provider();
+        var slot = ObjectSnapshots.snapshotRegion().getSlot(0);
+
+        try {
+
+            // Snapshot path
+            Path p = Paths.get("cache.doss");
+            File f = new File(p.toString());
+
+            ObjectSnapshot s = null;
+
+            // Check if snapshot exists
+            if (f.isFile()) { 
+                // If yes, then...
+                s = provider.load(p, slot);
+            } else {
+                // If not, then...
+
+                // Warm-up the cache
+                for (int i = 1; i <= 12; i++) {
+                    Headlines.instance().get(i);
+                }
+
+                // Store warmed-up cache
+                s = provider.createObjectSnapshot(Headlines.instance, slot);
+                provider.store(slot, p);
+            }
+
+            cache = (Headlines) s.get();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Cacheable
     public List<String> headlines(Integer month) {
-        return Headlines.instance().get(month);
+        return cache.get(month);
     }
 
     @CachePut(parameters = {"month"})
     public List<String> addHeadline(Integer month, String headline) {
-        Headlines.instance().put(month, headline);
-        return Headlines.instance().get(month);
+        cache.put(month, headline);
+        return cache.get(month);
     }
 
     @CacheInvalidate(parameters = {"month"})
     public void removeHeadline(Integer month, String headline) {
-        var l = Headlines.instance().get(month);
+        var l = cache.get(month);
         l.remove(headline);
-        Headlines.instance().replace(month, l);
+        cache.replace(month, l);
     }
 }
